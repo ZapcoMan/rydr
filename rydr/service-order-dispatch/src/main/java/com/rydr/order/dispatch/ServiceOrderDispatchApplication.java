b8d1997b7213c8ebde89451edd011d18dfe95e05
zapcoman <3217277135@qq.com>
2026-08-30 20:26:21 +0800

    refactor(microservices): 将Eureka客户端注解替换为通用发现客户端
    - 替换@EnableEurekaClient为@EnableDiscoveryClient以提高服务发现的通用性
    - 在ServiceValuationApplication中添加@MapperScan注解配置
    - 统一四个微服务应用中的服务注册发现配置方式
    - 提高代码的可移植性和对不同注册中心的支持能力

--- rydr/service-order-dispatch/src/main/java/com/rydr/order/dispatch/ServiceOrderDispatchApplication.java
+++ rydr/service-order-dispatch/src/main/java/com/rydr/order/dispatch/ServiceOrderDispatchApplication.java
@@ -2,12 +2,12 @@ package com.rydr.order.dispatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author oi
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceOrderDispatchApplication {
 
--- rydr/service-valuation/src/main/java/com/rydr/ServiceValuationApplication.java
+++ rydr/service-valuation/src/main/java/com/rydr/ServiceValuationApplication.java
@@ -1,13 +1,15 @@
 package com.rydr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @author oi
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.rydr.mapper")
 public class ServiceValuationApplication {
 
 	public static void main(String[] args) {
