2219476e36a9f0ebb663f8d1d333b1bdc9f80839
zapcoman <3217277135@qq.com>
2026-08-31 20:39:09 +0800

    feat(passenger-user): 添加通过手机号查询乘客信息功能
    - 引入 PassengerUserInfoCustomMapper 和 PassengerUserInfo 实体类
    - 注入 PassengerUserInfoCustomMapper 依赖
    - 实现 getByPhone 方法通过手机号查询乘客信息
    - 返回 ResponseResult 包装的乘客信息数据
    - 添加相应的 API 接口映射和注解配置

--- rydr/service-passenger-user/src/main/java/com/rydr/controller/PassengerUserController.java
+++ rydr/service-passenger-user/src/main/java/com/rydr/controller/PassengerUserController.java
@@ -1,12 +1,30 @@
 package com.rydr.controller;
 
 import com.rydr.dto.ResponseResult;
+import com.rydr.dao.mapper.PassengerUserInfoCustomMapper;
+import com.rydr.dao.entity.PassengerUserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author oi
+ */
 @RestController
 @RequestMapping("/passenger-user")
 public class PassengerUserController {
 
-//    public ResponseResult login
+    @Autowired
+    private PassengerUserInfoCustomMapper passengerUserInfoMapperCustom;
+
+    /**
+     * Query passenger profile by phone number.
+     */
+    @GetMapping("/get-by-phone/{phoneNumber}")
+    public ResponseResult<PassengerUserInfo> getByPhone(@PathVariable("phoneNumber") String phoneNumber) {
+        PassengerUserInfo userInfo = passengerUserInfoMapperCustom.selectByPhoneNumber(phoneNumber);
+        return ResponseResult.success(userInfo);
+    }
 }
