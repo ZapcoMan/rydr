a509ebeb197a959eb3f87d1633bd68bba95c7b85
zapcoman <3217277135@qq.com>
2026-08-31 20:35:21 +0800

    feat(address): 实现乘客地址查询功能
    - 添加 PassengerAddressMapper 注入
    - 实现通过乘客ID和地址类型查询地址列表功能
    - 返回成功响应包含地址信息
    - 完善地址控制器的数据访问逻辑

--- rydr/service-passenger-user/src/main/java/com/rydr/controller/AddressController.java
+++ rydr/service-passenger-user/src/main/java/com/rydr/controller/AddressController.java
@@ -1,10 +1,15 @@
 package com.rydr.controller;
 
+import com.rydr.dao.mapper.PassengerAddressMapper;
 import com.rydr.dto.ResponseResult;
+import com.rydr.entity.PassengerAddress;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author oi
@@ -13,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/address")
 public class AddressController {
 
+    @Autowired
+    private PassengerAddressMapper passengerAddressMapper;
+
     /**
      * Get address information by passenger ID and address type
      * @param passengerId
@@ -20,8 +28,8 @@ public class AddressController {
      * @return
      */
     @GetMapping("/get-address/{passengerId}/{type}")
-    public ResponseResult getAddress(@PathVariable("passengerId") int passengerId,@PathVariable("type") int type){
-        // Get passenger address information
-        return null;
+    public ResponseResult getAddress(@PathVariable("passengerId") int passengerId, @PathVariable("type") int type){
+        List<PassengerAddress> addresses = passengerAddressMapper.selectByPassengerAndType(passengerId, type);
+        return ResponseResult.success(addresses);
     }
 }
